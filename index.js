207c8bb282fa43d5e50c2efebdb6a89363e19212
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-10 12:47:00 -0800

    commit tree 2e36c539c54918b6548976d9551462409a86a510

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Lela is the champion";
+
+console.log(newHeader)
+
